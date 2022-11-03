08cda58460c8da4fec36dc06917f34fce69acd89
Fahimefto <ifto.141.fi@gmail.com>
2022-11-03 19:11:20 +0600

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,43 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useState } from "react";
+import Router from "next/router";
+import HashLoader from "react-spinners/HashLoader";
+
+const override = {
+  position: "absolute",
+  color: "#01cffc",
+  left: "50%",
+  top: "40%",
+  width: "100vw",
+  height: "100vh",
+  backRoundColor: "white",
+  transform: "translate(-50%, -50%)",
+  zIndex: "100",
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = useState(false);
+  Router.events.on("routeChangeStart", () => {
+    console.log("Router changing");
+    setLoading(true);
+  });
+  Router.events.on("routeChangeComplete", () => {
+    console.log("finised changing");
+    setLoading(false);
+  });
+  return (
+    <>
+      {loading && (
+        <HashLoader
+          color="#006747"
+          loading={loading}
+          cssOverride={override}
+          size={80}
+        />
+      )}
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
